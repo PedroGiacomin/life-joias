da19b54a80120234a5c25f275bb05b44d19076ac
Pedro <plsg2020.ufmg@gmail.com>
2021-10-14 08:57:29 -0300

    Troquei o componente AppBar por um Header simples
    Está estilizado do mesmo jeito

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {AppBar, IconButton, Toolbar, Typography, StyledInputBase} from "@material-ui/core"
+import {IconButton} from "@material-ui/core"
 import {BsPersonCircle, BsHandbag} from "react-icons/bs"
 
 import "./Menu.css"
@@ -9,19 +9,17 @@ function Menu(){
 
   return(
     <>
-      <AppBar position="static">
-        <Toolbar>
-          <img className="logo" src="./imagens/Logo.png"/>
-          <div className="iconContainer">
-            <IconButton>
-              <BsPersonCircle className="icone"/>
-            </IconButton>
-            <IconButton>
-              <BsHandbag className="icone"/>
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
+      <header className="cabecalho">
+        <img className="logo" src="./imagens/Logo.png"/>
+        <div className="iconContainer">
+          <IconButton>
+            <BsPersonCircle className="icone"/>
+          </IconButton>
+          <IconButton>
+            <BsHandbag className="icone"/>
+          </IconButton>
+        </div>
+      </header>
     </>
   );
 }
