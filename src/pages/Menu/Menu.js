9eb6647ed995fbbcb9931bac4161b8f4d4f722e1
Pedro <plsg2020.ufmg@gmail.com>
2021-10-14 09:54:49 -0300

    Botões semifuncionais
    Mudar os links para o qual eles redirecionam depois que as páginas estiverem montadas.

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,23 +1,46 @@
 import React from "react";
 import {IconButton} from "@material-ui/core"
-import {BsPersonCircle, BsHandbag} from "react-icons/bs"
+import {BsPersonCircle, BsHandbag, BsHeart} from "react-icons/bs"
+import { useHistory } from "react-router";
 
 import "./Menu.css"
 
 /**Menu é header + navbar. */
 function Menu(){
 
+  const history = useHistory();
+  
+  //Guarda se o cliente ja logou ou nao
+  //DUVIDA: Como fazer esse bool ter o valor que queremos
+  const isLogado = false;
+
+  //Funcao para ir pra pagina em um clique
+  function handleClick(pathName){
+    history.push(pathName);
+  }
+
+  //Define para que pag o botao vai mandar
+  function handleProfButton(logged){
+    const pathName = (logged ? "/perfil" : "/login");
+    handleClick(pathName);
+  }
+
   return(
     <>
       <header className="cabecalho">
         <img className="logo" src="./imagens/Logo.png"/>
         <div className="iconContainer">
-          <IconButton>
+          <IconButton onClick={() => handleProfButton(isLogado)}>
             <BsPersonCircle className="icone"/>
           </IconButton>
-          <IconButton>
+          {/*Por enquanto vai para o login, mas eh temporario*/}
+          <IconButton onClick={() => handleClick("/login")}>
             <BsHandbag className="icone"/>
           </IconButton>
+          <IconButton onClick={() => handleClick("/login")}>
+            <BsHeart className="icone"/>
+          </IconButton>
+          
         </div>
       </header>
     </>
