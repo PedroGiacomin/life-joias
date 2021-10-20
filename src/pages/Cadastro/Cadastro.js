f6c216d8cfd7e710a3bcde405158355ad328b57d
Monique <moniquecosta@cpejr.com.br>
2021-10-20 13:53:40 -0300

    ajuste

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+
 import "./Cadastro.css"
 
 function Cadastro(){
 
-  return(<h1>Cadastro</h1>);
+  return(
+  
+    <h1>Cadastro</h1>
+  <input type="text" placeholder="Nome"/>
+  <input type="text" placeholder="Email"/>
+    
+    
+    );
 }
 
 export default Cadastro;
