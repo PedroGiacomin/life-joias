4a3d6accfe13744c0e2ede18ad3b49ec6039ee91
Pedro Giacomin <67748410+PedroGiacomin@users.noreply.github.com>
2021-10-22 16:19:45 -0300

    Revert "Login primeira fase"

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,32 +1,10 @@
 import React from "react";
-import { Form } from "react-bootstrap";
-import "./Login.css";
 
-function Login() {
-  
-  
-  return(
-    <div className="base">
-      <div className = "container">
-        <Form>
-          <img src="/imagens/Logo.png" alt="Life Joias"></img> 
-          <div className="inputs">
-          <Form.Group className="mb-3" controlId="formGroupEmail">
-            <Form.Label></Form.Label>
-            <Form.Control type="email" placeholder="e-mail" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formGroupPassword">
-            <Form.Label></Form.Label>
-            <Form.Control type="password" placeholder="digite sua senha"  />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Esqueceu sua senha?</Form.Label>
-          </Form.Group>
-          <button variant="primary">ENTRAR</button>{'Não possui cadastro? Cadastre-se agora'}  
-          </div>
-        </Form>
-      </div>
-    </div>
-  );
+import "./Login.css"
+
+function Login(){
+
+  return(<h1>Login</h1>);
 }
+
 export default Login;
