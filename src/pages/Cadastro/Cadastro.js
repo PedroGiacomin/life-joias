125de8cd8b64542f1131e5c63793010ba494b0f7
Pedro <plsg2020.ufmg@gmail.com>
2021-10-22 10:21:53 -0300

    Form com useState salvando no objeto Pessoa

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,18 +1,90 @@
-import React from "react";
-
+import React, {useState} from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 import "./Cadastro.css"
 
 function Cadastro(){
 
-  return(
-  <>
-    <h1>Cadastro</h1>
-  <input type="text" placeholder="Nome"/>
-  <input type="text" placeholder="Email"/>
-  </>
+  //Vai salvar os dados inseridos
+  const [pessoa, setPessoa] = useState({});
+
+  function handleInputChange(e){
+    const key = e.target.name;
+
+    const newPessoa = {...pessoa};
+    newPessoa[key] = e.target.value;
     
-    );
+    setPessoa(newPessoa);
+  }
+
+  //Input basico: nome do dado + dado
+  function inputBox(nomeShow, nome){
+    return(
+    <>
+      <Form.Group className="mb-3" className="inputBoxCad">
+        <Form.Label className="nomeInputCad">{nomeShow}</Form.Label>
+        <Form.Control 
+          className="dadoInputCad"
+          type="input" 
+          name={nome}
+          onChange={handleInputChange}
+        />
+        {/**Inserir aqui uma logica para liberar a escrita somente quando
+        o usuario clicar no botao de atualizar dados*/}
+      </Form.Group>
+    </>
+    )
+  }  
+  
+  /* O que muda eh so o dadoInput */
+  function inputBoxShort(nomeShow, nome){
+    return(
+    <>
+      <Form.Group className="mb-3" className="inputBoxShortCad">
+        <Form.Label className="nomeInputCad">{nomeShow}</Form.Label>
+        <Form.Control 
+          className="dadoInputCad"
+          type="input" 
+          name={nome}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+    </>
+    )
+  }  
+
+  return(
+    <>
+      <div className="baseCad">
+        <div className="caixaCad">
+          <h2 className="titleCad">Cadastro</h2>
+         
+          <Form className="formCad">
+            
+            {inputBox("Nome", "nome")}
+            {inputBox("Email", "email")}
+            <div className="doisInputsCad">
+              {inputBoxShort("Telefone", "telefone")}
+              {inputBoxShort("Nascimento", "nascimento")} 
+            </div>
+            {inputBoxShort("CEP", "cep")}
+            {inputBox("Logradouro", "logradouro")}
+            <div className="doisInputsCad">   
+              {inputBoxShort("Número", "numero")} 
+              {inputBoxShort("Bairro", "bairro")}
+            </div>
+            <div className="doisInputsCad">
+              {inputBoxShort("Cidade", "cidade")} 
+              {inputBoxShort("Estado", "estado")}
+            </div>
+            
+          </Form>
+
+       </div>
+      </div>
+    </>  
+  );
 }
 
 export default Cadastro;
