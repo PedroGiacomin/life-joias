b788a008d6bcf51000e368cbf398dbb86d0b1da2
Juliana Silvia <julianaktarino@gmail.com>
2021-10-25 21:51:07 -0300

    Inicio desenvolvimento produtos

--- src/pages/Produtos/Produtos.js
+++ src/pages/Produtos/Produtos.js
@@ -4,27 +4,10 @@ import Cartao from "../../components/Cartao";
 
 import "./Produtos.css"
 
-const anel=[
-  {
-    id: "anel1",
-    nome: "Anel Splindow",
-    preco: 159.90,
-    img: "../../images/anel1.jpg"
-  },
-]
-
 function Produtos(){
 
-  return(
-    <div className="baseProdutos">
-    <div className = "containerProdutos">
-        <img src="/imagens/image1.png" width="320" height="205" alt="Life Joias"></img> 
-        <div className="inputsProdutos"></div>
-      </div>
-    </div>
-  );
-
-  
+  return(<h1>Semijóias</h1>);
+ 
   
 };
 
