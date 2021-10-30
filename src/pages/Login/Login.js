076f0ab54a807ca33b6b1873fcbd953bf1317b76
Juliana Silvia <julianaktarino@gmail.com>
2021-10-30 11:09:37 -0300

    Melhora estilo Login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -9,39 +9,70 @@ function Login() {
   const [password, setPassword] = useState();
   const history = useHistory();
   
-  function login(){
-    //temos que estabelecer um suposto usuario para demonstrar que o login funciona
+function Logar(){
+    
     if(email == "emailcadastrado@life.com.br" && password == "senhacadastrada"){
       alert("Bem vindo à Life Joias");
       history.push("home");
     }else alert("Acesso negado: dados incorretos");
-  }
+}
+
+function inputBoxEmail(nomeShow, nome){
+    return(
+    <>
+      <Form.Group className="mb-3" className="formGroupPassword">
+        <Form.Label className="nomeInputLogin">{nomeShow}</Form.Label>
+        <Form.Control 
+          className="dadoInputLogin"
+          type="input" 
+          name={nome}
+          
+        />
+       
+      </Form.Group>
+    </>
+    )
+}  
+
+function inputBoxSenha(nomeShow, nome){
+  return(
+  <>
+    <Form.Group className="mb-3" className="formGroupPassword">
+      <Form.Label className="nomeInputLogin">{nomeShow}</Form.Label>
+      <Form.Control 
+        className="dadoInputLogin"
+        type="password" 
+        name={nome}
+        
+      />
+     
+    </Form.Group>
+  </>
+  )
+}
 
 
   return(
-    <div className="baseLogin">
-      
-      <div className = "containerLogin">
-        <div className = "baseTitulo">
-      <h2>Login</h2>
+<div className="baseLogin">
+        <div className="caixaLogin">
+          <h2 className="titleLogin">Login</h2>
+         
+          <Form className="formLogin">
+            
+            {inputBoxEmail("Email", "email")}
+            <Form type="email" placeholder="e-mail" onChange={(e)=>setEmail(e.target.value)}
+            />
+            {inputBoxSenha("Senha", "senha")}
+            <Form type="password" placeholder="e-mail" onChange={(e)=>setPassword(e.target.value)}
+            />
+        
+          </Form>
 
-      </div>
-       
-          <Form.Group className="inputEspaco" controlId="formGroupEmail">
-            <Form.Label></Form.Label>
-            <Form.Control type="email" placeholder="e-mail" onChange={(e)=>setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <div className = ".inputEspaco"/>
-          <Form.Group className="mb-3" controlId="formGroupPassword">
-            <Form.Label></Form.Label>
-            <Form.Control type="password" placeholder="digite sua senha" onChange={(e)=>setPassword(e.target.value)} 
-            />
-          <div className = ".inputEspaco"/>
-          </Form.Group>
+    
           <Form.Group className="mb-3">
             <Form.Label>Esqueceu sua senha?</Form.Label>
           </Form.Group>
+          
           <Button className="botaoLogin" variant="primary" size="lg">
             Entrar
           </Button>{' '}
