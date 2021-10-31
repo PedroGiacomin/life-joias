01df7bb9695f8f15389c39aba471ede14634874c
Juliana Silvia <julianaktarino@gmail.com>
2021-10-30 23:56:45 -0300

    Melhorias reta final produto 1

--- src/pages/Produtos/Produtos.js
+++ src/pages/Produtos/Produtos.js
@@ -5,8 +5,6 @@ import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import Button from "react-bootstrap/Button";
 import Cartao from "../../components/Cartao";
 import CardGroup from 'react-bootstrap/CardGroup';
-
-
 import "./Produtos.css"
 
 function Produtos(){
@@ -14,37 +12,53 @@ function Produtos(){
   return(
   <div className="containerProdutos">
     <div className="baseFoto">
-      <h2>Anel Splindow</h2>
     
-      <img src="/imagens/anel1.jpg" width="320" height="205" alt="Anel Splindow"></img> 
+       <img src="/imagens/anel1.jpg" width="320" height="205" alt="Anel Splindow"></img> 
     </div>
+    
     <div className="containerInfos">
-    <Form.Label>Preço  </Form.Label>
-    <Form.Label>R$143,90</Form.Label>
-    <form className="inputsProdutos"/>
-    <Form.Group className="mb-3">
-            <Form.Label>Quantidade</Form.Label>
-          </Form.Group>
-          <Form.Control type="text" placeholder="Quantidade do produto" readOnly />  
-    <div className="containerInfos">
-    <Form.Label>Tamanho</Form.Label>      
+
+     <div className="containerNome">
+      <h2>Anel Splindow</h2>
+       </div>
+     <div className="dadosQuantPreco">
+     <form className="inputsProdutos">
+        <Form.Group className="mb-3">
+          <Form.Label>Quantidade</Form.Label>
+        </Form.Group>
+        <Form.Control type="text" placeholder="Quantidade do produto" readOnly />
+      </form>
+     
+     <div className="containerPreco">
+      <Form.Label>Preço  </Form.Label>
+      <Form.Label>R$143,90</Form.Label>
+      </div>
+
+      
+      </div>
+
+      <div className="containerTamanho">
+
+      <Form.Label>Tamanho</Form.Label>      
       <Form.Select aria-label="Default select example">
-      <option>Escolha o tamanho</option>
-      <option value="1">18</option>
-      <option value="2">21</option>
-      <option value="3">23</option>
+        <option>Escolha o tamanho</option>
+        <option value="1">18</option>
+        <option value="2">21</option>
+        <option value="3">23</option>
       </Form.Select>
-    </div>
-    <div className="containerBotao"></div>
-    <Button className="botaoCompra" variant="primary" size="lg">
+      </div>
+
+      
+      <div className="containerBotao"></div>
+        <Button className="botaoCompra" variant="primary" size="lg">
             Adicionar ao carrinho
-          </Button>{' '}
+        </Button>{' '}
+      </div>
+
+
+
     </div>
-  </div>
-  
-  )
-
-};
-
+  );
+}
 
 export default Produtos;
