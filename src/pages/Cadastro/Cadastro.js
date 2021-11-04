a182743ce4300e4a6ff1ae764e0148f92cb4b591
Pedro <plsg2020.ufmg@gmail.com>
2021-11-03 22:32:39 -0300

    Está cadastrando no db mas nao no firebase

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { useHistory } from "react-router";
+import api from "../../services/api";
 
 import "./Cadastro.css"
 
@@ -8,6 +10,7 @@ function Cadastro(){
 
   //Vai salvar os dados inseridos
   const [pessoa, setPessoa] = useState({});
+  const history = useHistory();
 
   function handleInputChange(e){
     const key = e.target.name;
@@ -21,7 +24,9 @@ function Cadastro(){
   //Input basico: nome do dado + dado
   function inputBox(nomeShow, nome){
     return(
+      
     <>
+      {console.log(pessoa)}
       <Form.Group className="mb-3" className="inputBoxCad">
         <Form.Label className="nomeInputCad">{nomeShow}</Form.Label>
         <Form.Control 
@@ -54,6 +59,43 @@ function Cadastro(){
     )
   }  
 
+  function inputBoxSenha(nomeShow, nome){
+    return(
+    <>
+      <Form.Group className="mb-3" className="inputBoxShortCad">
+        <Form.Label className="nomeInputCad">{nomeShow}</Form.Label>
+        <Form.Control 
+          className="dadoInputCad"
+          type="password" 
+          name={nome}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+    </>
+    )
+  }  
+
+  async function handleCadastro(p){
+    
+    try {
+      const response = await api.post('/users', p);
+      alert("Cadastro Finalizado!");
+      history.push('login');
+    } catch (error) {
+      alert("Algo deu errado!")
+    }
+  }
+
+  function finalizaCadastro(){
+    if(pessoa.user_senha !== pessoa.confirm){
+      alert("A confirmação deve ser igual à senha!");
+    }
+    else{
+      delete pessoa['confirm'];
+      handleCadastro(pessoa);
+    }
+  }
+
   return(
     <>
       <div className="baseCad">
@@ -62,31 +104,33 @@ function Cadastro(){
          
           <Form className="formCad">
             
-            {inputBox("Nome", "nome")}
-            {inputBox("Email", "email")}
+            {inputBox("Nome", "user_nome")}
+            {inputBox("Email", "user_email")}
             <div className="doisInputsCad">
-              {inputBoxShort("Telefone", "telefone")}
-              {inputBoxShort("Nascimento", "nascimento")} 
+              {inputBoxShort("Telefone", "user_telefone")}
+              {inputBoxShort("Nascimento", "user_nascimento")} 
             </div>
-            {inputBoxShort("CEP", "cep")}
-            {inputBox("Logradouro", "logradouro")}
+            {inputBoxShort("CEP", "user_cep")}
+            {inputBox("Logradouro", "user_rua")}
             <div className="doisInputsCad">   
-              {inputBoxShort("Número", "numero")} 
-              {inputBoxShort("Bairro", "bairro")}
+              {inputBoxShort("Número", "user_numero")} 
+              {inputBoxShort("Bairro", "user_bairro")}
             </div>
             <div className="doisInputsCad">
-              {inputBoxShort("Cidade", "cidade")} 
-              {inputBoxShort("Estado", "estado")}
+              {inputBoxShort("Cidade", "user_cidade")} 
+              {inputBoxShort("Estado", "user_estado")}
             </div>
 
             <br/>
-
-            {inputBox("Senha", "senha")}
-            {inputBox("Confirme sua senha", "confirm")}
+            <div className="doisInputsCad">
+              {inputBoxSenha("Senha", "user_senha")}
+              {inputBoxSenha("Confirme sua senha", "confirm")}
+            </div>
 
           </Form>
 
-          <Button className="botaoCad" variant="primary" size="lg">
+          <Button className="botaoCad" variant="primary" size="lg"
+            onClick={finalizaCadastro}>
             FINALIZAR
           </Button>{' '}
        </div>
